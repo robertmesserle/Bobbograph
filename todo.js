c98d84d9baf6c722d857ba0f840d10034b2927c6
robertmesserle <robertmesserle@gmail.com>
2014-02-26 00:01:19 -0800

    updated todo list

--- todo.js
+++ todo.js
@@ -1,4 +1,3 @@
-this.shadow            = obj.shadow                   || false;                             // boolean:     do you want a shadow to be applied?
 this.anim_duration     = obj.duration                 || 0;                                 // number:      duration of the animation (animation will be skipped if set to 0)
 this.callback          = obj.callback                 || false;                             // function:    function to be called after the animation completes
 this.normal_range      = max_min( obj.normal_range    || 0, 0, raw_data.length - 2 );       // number:      range to be used for moving average (0 means no normalization will occur)
